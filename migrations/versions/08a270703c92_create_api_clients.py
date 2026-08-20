f2b5e1487ef205ede8ffe798a3db6a611af66e16
dzldebby <debby.lingzl@gmail.com>
2026-08-20 21:55:51 +0800

    auto: 2026-08-20 21:55 | igrations/versions/08a270703c92_create_api_clients.py

--- migrations/versions/08a270703c92_create_api_clients.py
+++ migrations/versions/08a270703c92_create_api_clients.py
@@ -8,10 +8,8 @@ Create Date: 2026-08-20 21:55:33.996642
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
 revision: str = '08a270703c92'
 down_revision: Union[str, Sequence[str], None] = 'd4c165b49d52'
 branch_labels: Union[str, Sequence[str], None] = None
@@ -19,10 +17,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
-    pass
+    op.execute("""
+        CREATE TABLE api_clients (
+            client_id     TEXT        PRIMARY KEY,
+            api_key_hash  TEXT        NOT NULL UNIQUE,
+            created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
+        );
+    """)
 
 
 def downgrade() -> None:
-    """Downgrade schema."""
-    pass
+    op.execute("DROP TABLE IF EXISTS api_clients;")
